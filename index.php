e53b16071b75fd001e8724ffff3d0172d1943ac6
unknown <mahmoudnagah999@gmail.com>
2023-08-22 11:36:33 +0300

    create validation class and use it on username and email fields

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
-if (isset($_POST["submit"]))
-{
-    echo "Form Submited";
+    require('User_Validation.php');
 
-}
+    if (isset($_POST["submit"])) {
+        $validation = new UserValidator($_POST);
+        $errors = $validation->validateForm();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -18,10 +19,16 @@ if (isset($_POST["submit"]))
         <h2>Create New User</h2>
         <form action="<?= $_SERVER["PHP_SELF"]?>" method="POST">
             <label>Username</label>
-            <input type="text" name="username">
+            <input type="text" name="username" value="<?= htmlspecialchars($_POST['username'] ?? ''); ?>">
+            <div class="errors">
+                <?= $errors['username'] ?? '' ?>
+            </div>
 
             <label>Email</label>
-            <input type="text" name="email">
+            <input type="text" name="email" value="<?= htmlspecialchars($_POST['email'] ?? '') ?>">
+            <div class="errors">
+                <?= $errors['email'] ?? '' ?>
+            </div>
 
             <input type="submit" value="submit" name="submit">
         </form>
